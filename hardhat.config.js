3ddabaacfa52920210ad6b903d0bc7f1b3648693
François Liard <56561415+Pacdac@users.noreply.github.com>
2024-04-06 19:18:57 +0200

    update configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    xrpEVM: {
+      url: "https://rpc-evm-sidechain.xrpl.org/",
+      chainId: 1440002,
+      accounts: [process.env.EVM_PRIVATE_KEY]
+    }
+  }
 };
