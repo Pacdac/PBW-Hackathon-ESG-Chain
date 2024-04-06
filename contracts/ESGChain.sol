8a875bd645ae86eee84ac497c984e4f445f84c76
n3wil <29715535+n3wil@users.noreply.github.com>
2024-04-06 14:14:17 +0200

    can change weights

--- contracts/ESGChain.sol
+++ contracts/ESGChain.sol
@@ -8,6 +8,10 @@ it will return the score based on the data provided and the weightage given to e
 
 */
 
+// SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 pragma solidity ^0.8.0;
 
 contract ESGScore {
@@ -33,23 +37,35 @@ contract ESGScore {
     mapping(address => EnterpriseData) public enterpriseData;
 
     // Weights for each parameter
-    uint256 public E1_weight;
-    uint256 public E2_weight;
-    uint256 public E3_weight;
-    uint256 public S1_weight;
-    uint256 public S2_weight;
-    uint256 public S3_weight;
-    uint256 public G1_weight;
-    uint256 public G2_weight;
-    uint256 public G3_weight;
+    uint256 public E1_weight = 20;
+    uint256 public E2_weight = 15;
+    uint256 public E3_weight = 10;
+    uint256 public S1_weight = 15;
+    uint256 public S2_weight = 15;
+    uint256 public S3_weight = 10;
+    uint256 public G1_weight = 15;
+    uint256 public G2_weight = 10;
+    uint256 public G3_weight = 10;
 
     // Add an enterprise to the whitelist
-    function addWhitelistedEnterprise(address enterprise) public {
+    function addWhitelistedEnterprise(address enterprise) onlyOwner {
         whitelistedEnterprises.push(enterprise);
     }
 
+    // Remove an enterprise from the whitelist
+    function removeWhitelistedEnterprise(address enterprise) onlyOwner {
+        for (uint256 i = 0; i < whitelistedEnterprises.length; i++) {
+            if (whitelistedEnterprises[i] == enterprise) {
+                whitelistedEnterprises[i] = whitelistedEnterprises[whitelistedEnterprises.length - 1];
+                whitelistedEnterprises.pop();
+                break;
+            }
+        }
+    }
+
     // Set the data for an enterprise
-    function setEnterpriseData(address enterprise, uint256 E1_score, uint256 E2_score, uint256 E3_score, uint256 S1_score, uint256 S2_score, uint256 S3_score, uint256 G1_score, uint256 G2_score, uint256 G3_score) public {
+    function setEnterpriseData(uint256 E1_score, uint256 E2_score, uint256 E3_score, uint256 S1_score, uint256 S2_score, uint256 S3_score, uint256 G1_score, uint256 G2_score, uint256 G3_score) public {
+        address enterprise = msg.sender;
         require(isWhitelisted(enterprise), "Enterprise is not whitelisted");
         enterpriseData[enterprise] = EnterpriseData(E1_score, E2_score, E3_score, S1_score, S2_score, S3_score, G1_score, G2_score, G3_score);
     }
@@ -64,6 +80,20 @@ contract ESGScore {
         return false;
     }
 
+    // Set the weights for each parameter
+    function setWeights(uint256 E1, uint256 E2, uint256 E3, uint256 S1, uint256 S2, uint256 S3, uint256 G1, uint256 G2, uint256 G3) onlyOwner {
+        E1_weight = E1;
+        E2_weight = E2;
+        E3_weight = E3;
+        S1_weight = S1;
+        S2_weight = S2;
+        S3_weight = S3;
+        G1_weight = G1;
+        G2_weight = G2;
+        G3_weight = G3;
+    }
+
+
     // Calculate the ESG score for an enterprise
     function calculateESGScore(address enterprise) public view returns (uint256) {
         require(isWhitelisted(enterprise), "Enterprise is not whitelisted");
@@ -71,10 +101,8 @@ contract ESGScore {
         uint256 E_score = (E1_weight * data.E1_score + E2_weight * data.E2_score + E3_weight * data.E3_score) / (E1_weight + E2_weight + E3_weight);
         uint256 S_score = (S1_weight * data.S1_score + S2_weight * data.S2_score + S3_weight * data.S3_score) / (S1_weight + S2_weight + S3_weight);
         uint256 G_score = (G1_weight * data.G1_score + G2_weight * data.G2_score + G3_weight * data.G3_score) / (G1_weight + G2_weight + G3_weight);
-        uint256 E_weight = 50;
-        uint256 S_weight = 30;
-        uint256 G_weight = 20;
-        uint256 score = (E_weight * E_score + S_weight * S_score + G_weight * G_score) / (E_weight + S_weight + G_weight);
+
+        uint256 score = E_score + S_score + G_score;
         return score;
     }  
 }
