8f778bf4adec6034927e5c271dc419e283ccfebe
François Liard <56561415+Pacdac@users.noreply.github.com>
2024-04-06 23:53:03 +0200

    Update tailwind.config.ts

--- esg-chain-website/tailwind.config.ts
+++ esg-chain-website/tailwind.config.ts
@@ -8,10 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#FF6363",
+        secondary: "#FFFFFF",
+        tertiary: {
+          100: "#629C9E",
+          200: "#395b5c",
+        }
       },
     },
   },
